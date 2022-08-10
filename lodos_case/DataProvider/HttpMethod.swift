b6ea42bd6b1656a93debaaa5182927565d6ed8c6
sercan kaya <sercankaya@sercans-MacBook-Pro.local>
2022-08-10 22:03:41 +0300

    CASE-01 Data Provider Base

---
+++ lodos_case/DataProvider/HttpMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HttpMethod.swift
+//  lodos_case
+//
+//  Created by sercan kaya on 10.08.2022.
+//
+
+import Foundation
+
+enum HttpMethod : String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+    case put = "PUT"
+}
