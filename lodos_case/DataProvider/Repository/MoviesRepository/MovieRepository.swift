7d54311df9a2cc63024be50b7250b035a35d6c9a
sercan kaya <sercankaya@sercans-MacBook-Pro.local>
2022-08-10 23:37:46 +0300

    CASE-02 add movies Repository

---
+++ lodos_case/DataProvider/Repository/MoviesRepository/MovieRepository.swift
@@ -0,0 +1,28 @@
+//
+//  MovieRepository.swift
+//  lodos_case
+//
+//  Created by sercan kaya on 10.08.2022.
+//
+
+import Foundation
+
+final class MoviesRepository: MoviesRepositoryConracts {
+    
+    let moviesServices: MoviesDataSources
+    
+    init(moviesServices: MoviesDataSources) {
+        self.moviesServices = moviesServices
+    }
+    
+    func getMovies(search: String, page: Int, completion: @escaping (Result<Movie, NetworkError>) -> Void) {
+        moviesServices.getMovies(search: search, page: page) { result in
+            switch result {
+            case .success(let response):
+                completion(.success(response))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
