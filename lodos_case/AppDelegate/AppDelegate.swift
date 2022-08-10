8b316fdee3de52242fdb94968990916a946d7013
sercan kaya <sercankaya@sercans-MacBook-Pro.local>
2022-08-10 22:03:18 +0300

    CASE-01 appDelegate Configration

--- lodos_case/AppDelegate/AppDelegate.swift
+++ lodos_case/AppDelegate/AppDelegate.swift
@@ -10,15 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        let viewController = SplashViewController()
-            let bounds = UIScreen.main.bounds
-            self.window = UIWindow(frame: bounds)
-            self.window?.makeKeyAndVisible()
-            self.window?.rootViewController = viewController
         return true
     }
 }
