1a8286ed53c2fea6cc771f1065009179af1c00a4
sercan kaya <sercankaya@sercans-MacBook-Pro.local>
2022-08-11 15:00:00 +0300

    CASE-03 add chipView

---
+++ lodos_case/UIComponents/UIViews/ChipView.swift
@@ -0,0 +1,20 @@
+//
+//  ChipView.swift
+//  lodos_case
+//
+//  Created by sercan kaya on 11.08.2022.
+//
+
+import UIKit
+
+class ChipView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
