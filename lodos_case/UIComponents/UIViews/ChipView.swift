e1f375e499460242d5e90a53bf2736e54499035e
sercan kaya <sercankaya@sercans-MacBook-Pro.local>
2022-08-11 20:34:34 +0300

    CASE-04  add chipView

--- lodos_case/UIComponents/UIViews/ChipView.swift
+++ lodos_case/UIComponents/UIViews/ChipView.swift
@@ -9,12 +9,66 @@ import UIKit
 
 class ChipView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let backgroundColors: [UIColor] = [.blue, .purple, .orange, .yellow]
+    
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = .systemFont(ofSize: 12, weight: .medium)
+        titleLabel.textColor = .white
+        return titleLabel
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        configureContents()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public var setTitle: String? {
+        didSet {
+            if let title = setTitle {
+                titleLabel.text = "\(String(describing: title))⭐"
+            }
+        }
+    }
+    
+    public var currentIndex: Int = 0 {
+        didSet {
+            if currentIndex <= backgroundColors.count {
+                backgroundColor = backgroundColors[currentIndex]
+            } else {
+                backgroundColor = backgroundColors[0]
+            }
+        }
+    }
 }
+
+// MARK: - UILayout
+extension ChipView {
+    
+    private func addSubViews() {
+        addTitleLabel()
+    }
+    
+    private func addTitleLabel() {
+        addSubview(titleLabel)
+        titleLabel.horizontalToSuperview(insets: .horizontal(4))
+        titleLabel.centerInSuperview()
+    }
+}
+
+
+// MARK: - ConfigureContents
+extension ChipView {
+    
+    private func configureContents() {
+        height(32)
+        layer.cornerRadius = 10
+      
+    }
+}
+
